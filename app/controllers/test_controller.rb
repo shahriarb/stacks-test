2789dc9c9e054ee7e75c8670f93dd3199d1ff823
Luvic van Gool <vic@thecloudblocks.com>
2013-05-02 12:38:49 +0100

    redis

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,6 +4,8 @@ class TestController < ApplicationController
 	def index
 		@values = [ENV['JAVA_HOME'], ENV['TEST']]		
 		@hostname = Socket.gethostname
+
+		$redis.incr("test_key") if Rails.env.production?
 	end
 
 end
