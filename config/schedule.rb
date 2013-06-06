79b2d581eb897c582f5ec73947a353d4f2e6f923
Luvic van Gool <vic@thecloudblocks.com>
2013-06-06 11:00:26 +0100

    rename

---
+++ config/schedule.rb
@@ -0,0 +1,18 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :output, "/tmp/cron_log.log"
+
+every 1.minutes do
+  command "ls"
+end
+
+every 2.minutes do
+  rake "test:one_off"
+end
+
+# Learn more: http://github.com/javan/whenever
